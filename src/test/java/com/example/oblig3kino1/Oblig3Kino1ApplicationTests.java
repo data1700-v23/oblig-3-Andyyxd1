e6edd97489d81fc23f73de7cbe780b651cefe81e
Andyyxd1 <114660172+Andyyxd1@users.noreply.github.com>
2023-04-19 12:44:31 +0200

    opprettelse av filen

---
+++ src/test/java/com/example/oblig3kino1/Oblig3Kino1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oblig3kino1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oblig3Kino1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
